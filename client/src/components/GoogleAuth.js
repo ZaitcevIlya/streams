3b1fd8beb486f9988a492133abb673592ea01f37
ZaitcevIlya <intiguan@yahoo.com>
2020-01-14 16:35:35 +0300

    connect redux dev tools and fix a bug in auth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -17,7 +17,7 @@ class GoogleAuth extends Component {
   }
 
   onSignInClick = () => {
-    this.auth.signIn(this.auth.currentUser.get().getId());
+    this.auth.signIn();
   }
 
   onSignOutClick = () => {
@@ -26,7 +26,7 @@ class GoogleAuth extends Component {
 
   onAuthChange = (isSignedIn) => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
