218008c4ea3776e04910576a7e950b19b681dec6
ZaitcevIlya <intiguan@yahoo.com>
2020-02-04 21:12:16 +0300

    make modal reusable

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
+
+const ModalWrapper = props => {
+  return (
+    <div
+      onClick={() => history.push('/')}
+      className='ui dimmer modals visible active'
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className='ui standart modal visible active'
+      >
+        <div className='header'>{props.title}</div>
+        <div className='content'>{props.content}</div>
+        <div className='actions'>{props.actions}</div>
+      </div>
+    </div>
+  )
+}
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div className='ui dimmer modals visible active'>
-      <div className='ui standart modal visible active'>Super modal</div>
-    </div>,
+    <ModalWrapper {...props} />,
     document.querySelector('#modal')
   )
 }
